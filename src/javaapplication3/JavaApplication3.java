68d8f1d15b54260e5826aa21b26c619b00bea472
Andriel <134160567+AndrielHenrique@users.noreply.github.com>
2024-09-22 16:36:15 -0300

    Exercicio Matriz

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -1,66 +1,87 @@
 
 package javaapplication3;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class JavaApplication3 {
     public static void main(String[] args) {
-       Scanner leitor = new Scanner(System.in);
-       
-       //System.out.println("Digite algo aqui: ");
-       //String input=leitor.nextLine();
-       //System.out.println(input+" "+input);
-       
-       System.out.println("Escolha uma opcao de 1 a 4: ");
-       int op = leitor.nextInt();
-       
-       int [][] matriz;
-       switch(op){
-           case 1 -> {
-               System.out.println("Op 1 - Criar Matriz");
-               matriz=criarMatriz();
-            }
-           case 2 -> {
-               System.out.println("Op 2 - Preencher a Matriz");
-               preencherMatriz(matriz);
-            }
-           case 3 -> { 
-               System.out.println("Op3 - Mostrar a Matriz");
-               mostrarMatriz(matriz);
-            }
-           default -> System.out.println("nenhuma");
-       }
-       
-    }
-    public static int criarMatriz(){
         Scanner leitor = new Scanner(System.in);
-        System.out.println("Digite o tamanho da matriz: ");
-        int input = leitor.nextInt();
-        
-        System.out.println("digite as colunas da matriz: ");
-        int input2 = leitor.nextInt();
-        
-        int [][]matriz = new int[input][input2];
-        return matriz;
-        
-    }
-    
-    public static int preencherMatriz(int[][]mat){
-       for(int i=0; i<mat.length; i++){
-           System.out.println("Digite o valor: ");
-            for(int j = 0; j<[0].mat[0];j++){
-             System.out.println("Digite o valor: ");
-       }
-           return mat;
-           
-    }
-    
-    public static void mostrarMatriz(int[][] mat){
-        for(int i=0; i<mat.length; i++){
-            for(int j = 0; j<mat[0].length; j++){
-                System.out.println(mat[i][j]);
+
+        // System.out.println("Digite algo aqui: ");
+        // String input=leitor.nextLine();
+        // System.out.println(input+" "+input);
+
+        int matriz[][] = null;
+        while (true) {
+
+            System.out.println("Escolha uma opcao de 1 a 4: ");
+            int op = leitor.nextInt();
+
+            switch (op) {
+                case 1 -> {
+                    System.out.println("Op 1 - Criar Matriz");
+                    int[] tamanho = criarMatriz(leitor);
+                    System.out.println("A matriz tera " + tamanho[0] + "linhas e " + tamanho[1] + "colunas");
+                    matriz = new int[tamanho[0]][tamanho[1]];
+                }
+                case 2 -> {
+                    if (matriz != null) {
+                        System.out.println("Op 2 - Preencher a Matriz");
+                        preencherMatriz(matriz, leitor);
+                    } else {
+                        System.out.println("Primeiro crie a matriz na op1");
+                    }
+                }
+                case 3 -> {
+                    if (matriz != null) {
+                        System.out.println("Op3 - Mostrar a Matriz");
+                        mostrarMatriz(matriz);
+
+                    } else {
+                        System.out.println("Primeiro crie a matriz na op1");
+                    }
+                }
+
+                case 4 -> {
+                    System.out.println("Sair");
+                    leitor.close();
+                    return;
+                }
+                default -> System.out.println("Opcao invalida, escolha uma op de 1 a 4");
             }
         }
+
+    }
+
+    public static int[] criarMatriz(Scanner leitor) {
+
+        System.out.println("Digite o tamanho da matriz: ");
+        int inputLinhas = leitor.nextInt();
+
+        System.out.println("digite as colunas da matriz: ");
+        int inputColunas = leitor.nextInt();
+
+        return new int[] { inputLinhas, inputColunas };
+
+    }
+
+    public static void preencherMatriz(int[][] mat, Scanner leitor) {
+        for (int i = 0; i < mat.length; i++) {
+
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.println("Digite o numero pra preencher a linha [" + i + "] e a coluna [" + j + "]: ");
+                mat[i][j] = leitor.nextInt();
+
+            }
+        }
+    }
+
+    public static void mostrarMatriz(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.println(mat[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
