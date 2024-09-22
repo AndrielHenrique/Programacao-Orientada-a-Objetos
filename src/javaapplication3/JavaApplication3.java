b5fdc4ef271e2305173b639b38463a89913310c6
Andriel <134160567+AndrielHenrique@users.noreply.github.com>
2024-09-22 16:43:45 -0300

    finish

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -84,4 +84,12 @@ public class JavaApplication3 {
             System.out.println();
         }
     }
+
+    public static void apagarMatriz(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                mat[i][j] = 0;
+            }
+        }
+    }
 }
